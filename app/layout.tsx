106e5815b3abb810697cceb97800e7ee7e4dd7c3
skillerdev <singhkarthik174.com>
2024-12-13 22:59:46 +0530

    Added anitDevtoll

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,9 +18,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {children}
-        {/* <script dangerouslySetInnerHTML={{
+        <script dangerouslySetInnerHTML={{
           __html: `(${preventDevTools.toString()})();`
-        }} /> */}
+        }} />
       </body>
     </html>
   )
